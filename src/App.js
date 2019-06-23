3874e8a4aaf4eaa2a476fc92baacabdcd9101d7f
Kevmsutton <kevmsutton@gmail.com>
2019-06-23 13:39:57 +0100

    add r router

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <div className='app'>
-        <Navbar />
-        <Home />
-        <Footer />
-      </div>
+      <Router>
+        <div className='app'>
+          <Navbar />
+          <Route exact path='/' component={Home} />
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,40 +1,47 @@
 import React from 'react';
 import './Navbar.css';
 import MaterialIcon, { colorPalette } from 'material-icons-react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
   render() {
     return (
-      <div>
-        <div className='topNav'>
-          <p>HENNA</p>
-          <ul>
-            <li className='personal'>
-              <MaterialIcon icon='favorite_border' size='40' />
-            </li>
-            <li>
-              <MaterialIcon icon='work' size='40' />
-            </li>
-            <li>
-              <MaterialIcon icon='person' size='40' />
-            </li>
-          </ul>
-        </div>
+      <Router>
+        <div>
+          <div className='topNav'>
+            <p>
+              <Link style={{ textDecoration: 'none', color: 'black' }} to='/'>
+                HENNA
+              </Link>
+            </p>
+            <ul>
+              <li className='personal'>
+                <MaterialIcon icon='favorite_border' size='40' />
+              </li>
+              <li>
+                <MaterialIcon icon='work' size='40' />
+              </li>
+              <li>
+                <MaterialIcon icon='person' size='40' />
+              </li>
+            </ul>
+          </div>
 
-        <div className='topNavYellowUnderLine' />
-        <div className='categoryNav'>
-          <ul>
-            <li>Category 1</li>
-            <li>Category 2</li>
-            <li>Category 3</li>
-            <li>Category 4</li>
-            <li>Category 5</li>
-            <li>Category 6</li>
-            <li>Category 7</li>
-            <li>Category 8</li>
-          </ul>
+          <div className='topNavYellowUnderLine' />
+          <div className='categoryNav'>
+            <ul>
+              <li>Category 1</li>
+              <li>Category 2</li>
+              <li>Category 3</li>
+              <li>Category 4</li>
+              <li>Category 5</li>
+              <li>Category 6</li>
+              <li>Category 7</li>
+              <li>Category 8</li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
