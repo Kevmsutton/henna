1f35a6233dd315cbfa0a3e26c36fe1bd23266adb
Kevmsutton <kevmsutton@gmail.com>
2019-11-12 11:18:52 +0000

    move productList to product feed folder and create assets folder

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -10,136 +10,18 @@ import './ProductList.scss';
 import MaterialIcon from 'material-icons-react';
 import alterBasedOnDeviceWidth from '../alterBasedOnDeviceWidth.js';
 import { Link } from 'react-router-dom';
-import white_table from './white_table.png';
+// import white_table from './assets/white_table.png';
+import Products from '../productFeed.js';
 
 class ProductList extends React.Component {
   state = {
-    products: [
-      {
-        id: 1,
-        name: 'SideTable',
-        price: 1000,
-        category: 'Sofas',
-        material: 'wood',
-        colour: 'black',
-        description: 'Side table in grey and bronze',
-        image: white_table
-      },
-      {
-        id: 2,
-        name: 'SideTable',
-        price: 159,
-        category: 'Sofas',
-        material: 'metal',
-        colour: 'white',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 3,
-        name: 'SideTable',
-        price: 2000,
-        category: 'Lighting',
-        material: 'wood',
-        colour: 'white',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 4,
-        name: 'SideTable',
-        price: 159,
-        category: 'Lighting',
-        material: 'plastic',
-        colour: 'white',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 5,
-        name: 'SideTable',
-        price: 209,
-        category: 'Lighting',
-        material: 'chrome',
-        colour: 'black',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 6,
-        name: 'SideTable',
-        price: 209,
-        category: 'Lighting',
-        material: 'chrome',
-        colour: 'black',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 7,
-        name: 'SideTable',
-        price: 209,
-        category: 'Lighting',
-        material: 'wood',
-        colour: 'black',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 8,
-        name: 'SideTable',
-        price: 109,
-        category: 'Lighting',
-        material: 'wood',
-        colour: 'black',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 9,
-        name: 'SideTable',
-        price: 162,
-        category: 'Lighting',
-        material: 'wood',
-        colour: 'black',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 10,
-        name: 'SideTable',
-        price: 109,
-        category: 'Lighting',
-        material: 'wood',
-        colour: 'black',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      },
-      {
-        id: 11,
-        name: 'WoodTable',
-        price: 109,
-        category: 'Lighting',
-        material: 'wood',
-        colour: 'black',
-        description: 'Side table in grey and bronze',
-        image:
-          'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
-      }
-    ],
+    products: Products,
     window: window.innerWidth
   };
 
   filteredProducts = () => {
     console.log(this.props.materialSelector);
+    console.log(Products);
     // if their is a price i only want to show products less than that price
     // if their is a material selected i only want to show products with that material
     // if their is a colour selected only products with that colour
@@ -234,30 +116,32 @@ class ProductList extends React.Component {
             </li>
           </ul>
         </div>
-        <div className='productBox'>
-          {this.filteredProducts().map(product => (
-            <div
-              key={product.id}
-              onClick={() => handleProductClick(product)}
-              className='individualProduct'
-            >
-              <Link
-                style={{ textDecoration: 'none', color: 'black' }}
-                to={`/${product.name}`}
-                key={product.name}
+        <div className='productBoxWrapper'>
+          <div className='productBox'>
+            {this.filteredProducts().map(product => (
+              <div
+                key={product.id}
+                onClick={() => handleProductClick(product)}
+                className='individualProduct'
               >
-                <img
-                  src={product.image}
-                  width='80%'
-                  height='85%'
-                  alt='heroImg'
-                />
-                <p>
-                  {product.name} - £{product.price}
-                </p>
-              </Link>
-            </div>
-          ))}
+                <Link
+                  style={{ textDecoration: 'none', color: 'black' }}
+                  to={`/${product.name}`}
+                  key={product.name}
+                >
+                  <img
+                    src={product.image}
+                    width='80%'
+                    height='85%'
+                    alt='heroImg'
+                  />
+                  <p>
+                    {product.name} - £{product.price}
+                  </p>
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
---
+++ src/Components/productFeed.js
@@ -0,0 +1,125 @@
+import white_table from '../assets/white_table.png';
+
+const Products = [
+  {
+    id: 1,
+    name: 'SideTable',
+    price: 1000,
+    category: 'Sofas',
+    material: 'wood',
+    colour: 'black',
+    description: 'Side table in grey and bronze',
+    image: white_table
+  },
+  {
+    id: 2,
+    name: 'SideTable',
+    price: 159,
+    category: 'Sofas',
+    material: 'metal',
+    colour: 'white',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 3,
+    name: 'SideTable',
+    price: 2000,
+    category: 'Lighting',
+    material: 'wood',
+    colour: 'white',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 4,
+    name: 'SideTable',
+    price: 159,
+    category: 'Lighting',
+    material: 'plastic',
+    colour: 'white',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 5,
+    name: 'SideTable',
+    price: 209,
+    category: 'Lighting',
+    material: 'chrome',
+    colour: 'black',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 6,
+    name: 'SideTable',
+    price: 209,
+    category: 'Lighting',
+    material: 'chrome',
+    colour: 'black',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 7,
+    name: 'SideTable',
+    price: 209,
+    category: 'Lighting',
+    material: 'wood',
+    colour: 'black',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 8,
+    name: 'SideTable',
+    price: 109,
+    category: 'Lighting',
+    material: 'wood',
+    colour: 'black',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 9,
+    name: 'SideTable',
+    price: 162,
+    category: 'Lighting',
+    material: 'wood',
+    colour: 'black',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 10,
+    name: 'SideTable',
+    price: 109,
+    category: 'Lighting',
+    material: 'wood',
+    colour: 'black',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  },
+  {
+    id: 11,
+    name: 'WoodTable',
+    price: 109,
+    category: 'Lighting',
+    material: 'wood',
+    colour: 'black',
+    description: 'Side table in grey and bronze',
+    image:
+      'https://res-4.cloudinary.com/made-com/image/upload/a_auto,b_transparent,c_pad,d_made.svg,dpr_1.0,f_auto,h_550,q_auto:best,w_1050/v4/catalog/product/asset/8/a/e/f/8aefd9f76750e7c9a1ac2162f93a338ca3819c20_TBLCAT020NAT_UK_Catania_Garden_Bar_Polywood_LB01.jpg'
+  }
+];
+
+export default Products;
